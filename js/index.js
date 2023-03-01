4b21b61b15185728e438af8d80803febbbc26ec8
mohamed saada <mohamed.saada127@gmail.com>
2023-03-01 04:49:59 +0200

    added comments

--- js/index.js
+++ js/index.js
@@ -1,12 +1,15 @@
 async function game() {
-  const loader = document.querySelector(".loader");
+  // getting the game data element
   const gameData = document.querySelector(".game-data");
+  // getting the game details element
   const gameDetails = document.querySelector(".game-details");
+  // getting the categories links
   const categories = document.querySelectorAll(".nav-link");
+  // starting game catergory to be shown
   let category = "MMORPG";
+  await getDataByCategory(category);
 
-  const data = await getDataByCategory(category);
-
+  // adding active class to the current clicked button
   categories.forEach((category) => {
     category.addEventListener("click", async (e) => {
       categories.forEach((category) => {
@@ -36,6 +39,7 @@ async function game() {
   });
 }
 
+// display the a list of game by a certain category
 function displayData(data) {
   let carton = ``;
   for (let i = 0; i < data.length; i++) {
@@ -60,6 +64,7 @@ function displayData(data) {
   document.querySelector(".game-list").innerHTML = carton;
 }
 
+// fetching the API by a certain category then passing the data to the displayData() to be shown
 async function getDataByCategory(category) {
   const loader = document.querySelector(".loader");
   const gameData = document.querySelector(".game-data");
@@ -85,6 +90,7 @@ async function getDataByCategory(category) {
   displayData(data);
 }
 
+// fetching the API by a certain id to get details about a game then passing the data to the displayGameDetails() to be shown
 async function getDetailsById(id) {
   const gameDetails = document.querySelector(".game-details");
   const loader = document.querySelector(".loader");
@@ -106,11 +112,12 @@ async function getDetailsById(id) {
   let data = await res.json();
   if (data) {
     loader.classList.replace("d-block", "d-none");
-    gameDetails.classList.replace("d-none", "d-block");
+    // gameDetails.classList.replace("d-none", "d-block");
   }
   displayGameDetails(data);
 }
 
+// display the a list of game by id
 function displayGameDetails(data) {
   let carton = ``;
 
@@ -121,15 +128,15 @@ function displayGameDetails(data) {
             <div class="game-details-content">
               <h3>Title:${data.title}</h3>
               <p>
-                category:
+                Category:
                 <span class="badge bg-danger">${data.genre}</span>
               </p>
               <p>
-                category:
+                Platform:
                 <span class="badge bg-danger">${data.platform}</span>
               </p>
               <p>
-                category:
+                Status:
                 <span class="badge bg-danger">${data.status}</span>
               </p>
               <p>
